985e4c6ef7d9687c8edfdc943c71cddddb48b5e6
u_mulder <m264695502@gmail.com>
2023-12-14 22:41:25 +0300

    Add docker-compose start/stop recipes

--- local.mk
+++ local.mk
@@ -1,4 +1,24 @@
 # Variables
+DOCKER_COMPOSE=docker-compose
+DOCKER_COMPOSE_FILE=docker-compose.yaml
+DOCKER_ENV_FILE=docker.env
 
 # Recipes
 ## Docker and docker-compose commands
+##--- Starting/stopping containers ---
+
+.PHONY: up
+up:
+	@echo "Starting up the containers"
+	@$(DOCKER_COMPOSE) --env-file $(DOCKER_ENV_FILE) -f $(DOCKER_COMPOSE_FILE) up -d
+
+.PHONY: down
+down:
+	@echo "Stopping the containers"
+	@$(DOCKER_COMPOSE) --env-file $(DOCKER_ENV_FILE) -f $(DOCKER_COMPOSE_FILE) down
+
+.PHONY: start
+start: up
+
+.PHONY: stop
+stop: down
